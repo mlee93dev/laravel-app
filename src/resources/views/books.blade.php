dd7682a153516c23eaca2b10d9636affa9a6da0e
Mark Lee <marklee@Marks-MacBook-Pro.local>
2024-04-18 20:54:26 -0400

    added bootstrap to app and books; init errors view; created views common and layouts folders

--- src/resources/views/books.blade.php
+++ src/resources/views/books.blade.php
@@ -1,11 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Books</title>
-</head>
-<body>
-  {{ $books }}
-</body>
-</html>
+@extends('layouts.app')
+ 
+@section('content')
+  
+    <div class="panel-body">
+
+        @include('common.errors')
+
+        <!-- New Book Form -->
+        <form action="{{ url('book') }}" method="POST" class="form-horizontal">
+            {{ csrf_field() }}
+ 
+            <!-- Book Name -->
+            <div class="form-group">
+                <label for="book" class="col-sm-3 control-label">Book</label>
+ 
+                <div class="col-sm-6">
+                    <input type="text" name="name" id="book-name" class="form-control">
+                </div>
+            </div>
+ 
+            <!-- Add Book Button -->
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-6">
+                    <button type="submit" class="btn btn-default">
+                        <i class="fa fa-plus"></i> Add Book
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+ 
+    <!-- TODO: Current Tasks -->
+@endsection
