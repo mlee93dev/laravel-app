d800091ebb86101a2cd9c0520ab780b3d184076e
Mark Lee <marklee@Marks-MacBook-Pro.local>
2024-04-18 22:48:22 -0400

    app title and error msg change

--- src/resources/views/common/errors.blade.php
+++ src/resources/views/common/errors.blade.php
@@ -1,7 +1,7 @@
 @if (count($errors) > 0)
     <!-- Form Error List -->
     <div class="alert alert-danger">
-        <strong>Whoops! Something went wrong!</strong>
+        <strong>An error has occurred.</strong>
  
         <br><br>
  
