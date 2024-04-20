7d60ac040bde0b837f1b3fbe9340eccf46240baf
Mark Lee <marklee@Marks-MacBook-Pro.local>
2024-04-20 15:34:00 -0400

    edit, save, cancel buttons and front end logic

--- src/resources/views/books.blade.php
+++ src/resources/views/books.blade.php
@@ -42,6 +42,37 @@
 
     </div>
  
+    <script>
+      function editAuthor(id)
+      {
+          document.getElementById(`author-span-${id}`).classList.add("hidden");
+          document.getElementById(`author-input-${id}`).classList.remove("hidden");
+          document.getElementById(`author-edit-${id}`).classList.add("hidden");
+          document.getElementById(`author-save-${id}`).classList.remove("hidden");
+          document.getElementById(`author-cancel-${id}`).classList.remove("hidden");
+      }
+
+      function saveAuthor(id)
+      {
+          document.getElementById(`author-span-${id}`).innerText = document.getElementById(`author-input-${id}`).value;
+
+          document.getElementById(`author-span-${id}`).classList.remove("hidden");
+          document.getElementById(`author-input-${id}`).classList.add("hidden");
+          document.getElementById(`author-edit-${id}`).classList.remove("hidden");
+          document.getElementById(`author-save-${id}`).classList.add("hidden");
+          document.getElementById(`author-cancel-${id}`).classList.add("hidden");
+      }
+
+      function cancelEditAuthor(id)
+      {
+          document.getElementById(`author-span-${id}`).classList.remove("hidden");
+          document.getElementById(`author-input-${id}`).classList.add("hidden");
+          document.getElementById(`author-edit-${id}`).classList.remove("hidden");
+          document.getElementById(`author-save-${id}`).classList.add("hidden");
+          document.getElementById(`author-cancel-${id}`).classList.add("hidden");
+      }
+    </script>
+
     <!-- Books List -->
     @if (count($books) > 0)
         <div class="panel panel-default"> 
@@ -59,12 +90,33 @@
                     @foreach ($books as $book)
                         <tr>
                             <!-- Book Title -->
-                            <td class="col-sm-7">
-                                <div>{{ $book->title }}</div>
+                            <td class="table-text col-sm-7">
+                                <div class="">{{ $book->title }}</div>
                             </td>
 
-                            <td class="table-text">
-                                <div>{{ $book->author }}</div>
+                            <td class="table-text col-sm-4">
+                              <form action="" method="POST">
+                                {{ csrf_field() }}
+                                {{ method_field('PATCH') }}
+                                <span id="author-span-{{ $book->id }}">{{ $book->author }}</span>
+                                <input id="author-input-{{ $book->id }}" class="hidden" type="text" value="{{ $book->author }}">
+
+                                <div class="pull-right hidden" id="author-cancel-{{ $book->id }}">
+                                  <button onclick="cancelEditAuthor({{ $book->id }});" type="button" class="btn btn-danger">
+                                    <i class="fa fa-times-circle-o" aria-hidden="true"></i>
+                                  </button>
+                                </div>
+                                <div class="pull-right hidden" id="author-save-{{ $book->id }}">
+                                  <button onclick="saveAuthor({{ $book->id }});" type="button" class="btn btn-info">
+                                    <i class="fa fa-floppy-o" aria-hidden="true"></i>
+                                  </button>
+                                </div>
+                                <div class="pull-right" id="author-edit-{{ $book->id }}">
+                                  <button onclick="editAuthor({{ $book->id }});" type="button" class="btn btn-info">
+                                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                                  </button>
+                                </div>
+                              </form>
                             </td>
 
                             <!-- Delete Button -->
