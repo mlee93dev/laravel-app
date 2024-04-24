7c02b01c3adae17945f63890efd4c22b276aacfb
Mark Lee <marklee@Marks-MacBook-Pro.local>
2024-04-23 23:47:42 -0400

    search test complete

---
+++ src/tests/Feature/SearchBookTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Book;
+
+class SearchTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testSearchBooks()
+    {
+        $book = ['title' => 'foo', 'author' => 'bar'];
+        $books = factory(Book::class)->create($book);
+
+        $response = $this->get('/search?search=foo');
+        $response->assertSuccessful();
+        $response->assertViewIs('books');
+        $response->assertViewHas(['books' => $books->get()]);
+    }
+}
